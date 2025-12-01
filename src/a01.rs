d62860be34cfcccbde5326f83383f8579fb086d5
red <red@kinda.red>
2025-12-01 22:16:53 +0100

    speed up a1

--- src/a01.rs
+++ src/a01.rs
@@ -1,16 +1,27 @@
-pub const SAMPLE_OUTPUT: i64 = 0;
+pub const SAMPLE_OUTPUT: i64 = 3;
 
 pub fn run(inp: &str) -> i64 {
     let mut state = 50;
     let mut zero_count = 0;
 
-    for line in inp.lines() {
-        if line.is_empty() {
-            continue;
-        }
+    let mut reader = inp.as_bytes().iter();
 
-        let multiplier = if line.starts_with('R') { 1 } else { -1 };
-        let modifier = line[1..].parse::<i64>().unwrap();
+    while let Some(c) = reader.next() {
+        // if *c == b'\n' {
+        //     continue;
+        // }
+
+        let multiplier = if *c == b'R' { 1 } else { -1 };
+        let mut modifier: i64 = (unsafe { reader.next().unwrap_unchecked() } - b'0') as i64;
+
+        #[allow(clippy::while_let_on_iterator)]
+        while let Some(&ch) = reader.next() {
+            if ch == b'\n' {
+                break;
+            }
+
+            modifier = modifier * 10 + (ch - b'0') as i64;
+        }
 
         state += modifier * multiplier;
         state = state.rem_euclid(100);
@@ -18,7 +29,7 @@ pub fn run(inp: &str) -> i64 {
             zero_count += 1;
         }
 
-        println!("Line: {}, State: {}", line, state);
+        // println!("Line: {}, State: {}", line, state);
     }
 
     zero_count
