329cdfcc76f1a3b7b5e0e42cf1ae31fde968bb06
red <red@kinda.red>
2025-12-02 23:13:50 +0100

    fast-ish stupid day 2

--- src/a02.rs
+++ src/a02.rs
@@ -15,7 +15,7 @@ const fn get_valid_ranges() -> [(i64, i64); 9] {
     ranges
 }
 
-pub fn run(inp: &str) -> i64 {
+pub fn run_naive(inp: &str) -> i64 {
     let valid_ranges = get_valid_ranges();
 
     let summed = inp
@@ -55,6 +55,78 @@ pub fn run(inp: &str) -> i64 {
                         let left = s.chars().take(s.len() / 2);
                         let right = s.chars().skip(s.len() / 2);
                         if left.zip(right).all(|(a, b)| a == b) {
+                            range_sum += num;
+                        }
+                    }
+                }
+            }
+            range_sum
+        })
+        .sum::<i64>();
+
+    summed
+}
+
+fn stringify_num_fast(num: u64) -> ([u8; 19], usize) {
+    let mut buf = [0u8; 19];
+    let mut n = num;
+    let mut idx = 0;
+
+    while n > 0 {
+        buf[idx] = (n % 10) as u8;
+        idx += 1;
+        n /= 10;
+    }
+
+    (buf, idx)
+}
+
+use rayon::iter::{ParallelBridge as _, ParallelIterator as _};
+pub fn run(inp: &str) -> i64 {
+    let valid_ranges = get_valid_ranges();
+
+    let input_stripped = &inp[0..inp.len() - 1];
+
+    let summed = input_stripped
+        .split(',')
+        .par_bridge()
+        .map(|range| {
+            if range.trim().is_empty() {
+                return 0;
+            }
+
+            let mut r = range.split('-');
+            let lower = r.next().unwrap().parse::<i64>().unwrap();
+            let upper = r.next().unwrap().parse::<i64>().unwrap();
+
+            let mut range_sum = 0;
+
+            for (valid_low, valid_high) in valid_ranges.iter() {
+                // check for any overlaps
+                if lower <= *valid_high && upper >= *valid_low {
+                    // take this range
+                    let actual_low = if lower > *valid_low {
+                        lower
+                    } else {
+                        *valid_low
+                    };
+                    let actual_high = if upper < *valid_high {
+                        upper
+                    } else {
+                        *valid_high
+                    };
+
+                    let range = actual_low..=actual_high;
+                    // split into two
+                    for num in range {
+                        // let s = num.to_string();
+
+                        let (string, len) = stringify_num_fast(num as u64);
+                        let s = &string[0..len];
+
+                        let left = &s[..s.len() / 2];
+                        let right = &s[s.len() / 2..];
+                        if left == right {
                             range_sum += num;
                         }
                     }
--- src/b02.rs
+++ src/b02.rs
@@ -22,31 +22,7 @@ pub const SAMPLE_OUTPUT: i64 = 4174379265;
 //     ranges
 // }
 
-// prime decomposition of 1-19 in a lookup table, but only include every prime once
-const fn repeatable_sections() -> [[u8; 5]; 20] {
-    let mut table = [[0u8; 5]; 20];
-    table[2] = [1, 0, 0, 0, 0];
-    table[3] = [1, 0, 0, 0, 0];
-    table[4] = [1, 2, 0, 0, 0];
-    table[5] = [1, 0, 0, 0, 0];
-    table[6] = [1, 2, 3, 0, 0];
-    table[7] = [1, 0, 0, 0, 0];
-    table[8] = [1, 2, 4, 0, 0];
-    table[9] = [1, 3, 0, 0, 0];
-    table[10] = [1, 5, 2, 0, 0];
-    table[11] = [1, 0, 0, 0, 0];
-    table[12] = [1, 2, 3, 4, 6];
-    table[13] = [1, 0, 0, 0, 0];
-    table[14] = [1, 7, 2, 0, 0];
-    table[15] = [1, 5, 3, 0, 0];
-    table[16] = [1, 2, 4, 8, 0];
-    table[17] = [1, 0, 0, 0, 0];
-    table[18] = [1, 2, 3, 6, 9];
-    table[19] = [1, 0, 0, 0, 0];
-    table
-}
-
-pub fn run(inp: &str) -> i64 {
+pub fn run_naive(inp: &str) -> i64 {
     // let valid_ranges = get_valid_ranges();
 
     let prime_lookup = repeatable_sections();
@@ -56,10 +32,6 @@ pub fn run(inp: &str) -> i64 {
         .unwrap()
         .split(',')
         .map(|range| {
-            if range.trim().is_empty() {
-                return 0;
-            }
-
             let mut r = range.split('-');
             let lower = r.next().unwrap().parse::<i64>().unwrap();
             let upper = r.next().unwrap().parse::<i64>().unwrap();
@@ -96,4 +68,128 @@ pub fn run(inp: &str) -> i64 {
         .sum::<i64>();
 
     summed
+}
+
+#[test]
+fn test_fast_stringify() {
+    let (s, len) = stringify_num_fast(1234567890);
+    let s_str: String = s[..len as usize]
+        .iter()
+        .rev()
+        .map(|&c| (c + b'0') as char)
+        .collect();
+    assert_eq!(s_str, "1234567890");
+}
+
+#[test]
+fn test_fast_stringify_range() {
+    for i in 1..100_000 {
+        let (s, len) = stringify_num_fast(i);
+        let s_str: String = s[..len as usize]
+            .iter()
+            .rev()
+            .map(|&c| (c + b'0') as char)
+            .collect();
+        assert_eq!(s_str, i.to_string());
+    }
+}
+
+use rayon::iter::{ParallelBridge as _, ParallelIterator as _};
+
+// prime decomposition of 1-19 in a lookup table, but only include every prime once
+const fn repeatable_sections() -> [[u8; 5]; 20] {
+    let mut table = [[0u8; 5]; 20];
+    table[2] = [1, 0, 0, 0, 0];
+    table[3] = [1, 0, 0, 0, 0];
+    table[4] = [1, 2, 0, 0, 0];
+    table[5] = [1, 0, 0, 0, 0];
+    table[6] = [1, 2, 3, 0, 0];
+    table[7] = [1, 0, 0, 0, 0];
+    table[8] = [1, 2, 4, 0, 0];
+    table[9] = [1, 3, 0, 0, 0];
+    table[10] = [1, 5, 2, 0, 0];
+    table[11] = [1, 0, 0, 0, 0];
+    table[12] = [1, 2, 3, 4, 6];
+    table[13] = [1, 0, 0, 0, 0];
+    table[14] = [1, 7, 2, 0, 0];
+    table[15] = [1, 5, 3, 0, 0];
+    table[16] = [1, 2, 4, 8, 0];
+    table[17] = [1, 0, 0, 0, 0];
+    table[18] = [1, 2, 3, 6, 9];
+    table[19] = [1, 0, 0, 0, 0];
+    table
+}
+
+fn stringify_num_fast(num: u64) -> ([u8; 19], usize) {
+    let mut buf = [0u8; 19];
+    let mut n = num;
+    let mut idx = 0;
+
+    while n > 0 {
+        buf[idx] = (n % 10) as u8;
+        idx += 1;
+        n /= 10;
+    }
+
+    (buf, idx)
+}
+
+pub fn run(inp: &str) -> i64 {
+    // let valid_ranges = get_valid_ranges();
+
+    let prime_lookup = repeatable_sections();
+
+    let input_stripped = &inp[0..inp.len() - 1];
+
+    let summed = input_stripped
+        .split(',')
+        .par_bridge()
+        .map(|range| {
+            let mut r = range.split('-');
+            let lower = r.next().unwrap().parse::<u64>().unwrap();
+            let upper = r.next().unwrap().parse::<u64>().unwrap();
+
+            // let lower = 2121212121;
+            // let upper = 2121212124;
+
+            let range = lower..=upper;
+            let range_sum: u64 = range
+                .into_iter()
+                .map(|num| {
+                    // let string = num.to_string();
+
+                    let (string, len) = stringify_num_fast(num);
+                    let s = &string[0..len];
+
+                    let mut local_sum = 0;
+
+                    for prime in prime_lookup[s.len()].iter().take_while(|&&p| p != 0) {
+                        let mut num_prime_valid = true;
+                        let first = &s[0..*prime as usize];
+                        for i in 1..(s.len() as u8 / *prime) {
+                            let start = i * prime;
+                            let end = start + prime;
+                            let part = &s[start as usize..end as usize];
+                            if part != first {
+                                num_prime_valid = false;
+                                break;
+                            }
+                        }
+
+                        if num_prime_valid {
+                            local_sum += num;
+                            // println!("Found valid number: {}", num);
+                            break;
+                        }
+                    }
+
+                    local_sum
+                })
+                .sum();
+
+            range_sum
+        })
+        .sum::<u64>();
+
+    summed as i64
 }
