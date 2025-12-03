950598f707b691862b2f58c29949260e07fae8a9
red <red@kinda.red>
2025-12-03 15:03:11 +0100

    day 3 fast-ish

--- src/a03.rs
+++ src/a03.rs
@@ -7,7 +7,7 @@ pub const SAMPLE_OUTPUT: i64 = 357;
 818181911112111
 */
 
-pub fn run(inp: &str) -> i64 {
+pub fn run_naive(inp: &str) -> i64 {
     inp.lines()
         .map(|line| {
             let mut max_digit = '0';
@@ -39,4 +39,51 @@ pub fn run(inp: &str) -> i64 {
             r
         })
         .sum::<u32>() as i64
+}
+
+pub fn run(inp: &str) -> i64 {
+    let lines: &[[u8; 101]; 200] = unsafe {
+        inp.as_bytes()
+            .as_chunks_unchecked::<101>()
+            .try_into()
+            .unwrap_unchecked()
+    };
+
+    lines
+        .iter()
+        .map(|line| {
+            let mut max_digit = b'0';
+            let mut max_digit_index = 0;
+
+            for (ind, c) in line.iter().take(line.len() - 2).enumerate() {
+                if *c > max_digit {
+                    max_digit = *c;
+                    max_digit_index = ind;
+                }
+            }
+
+            // find largest digit, located after first ind
+
+            let mut second_max_digit = b'0';
+            for c in line
+                .iter()
+                .skip(max_digit_index + 1)
+                .take(line.len() - max_digit_index - 2)
+            {
+                if *c > second_max_digit {
+                    second_max_digit = *c;
+                }
+            }
+
+            // let r = max_digit.to_digit(10).unwrap() * 10 + second_max_digit.to_digit(10).unwrap();
+            let r = (max_digit - b'0') as u16 * 10 + (second_max_digit - b'0') as u16;
+
+            // println!(
+            //     "line: {}, max: {}, second max: {}, r: {}",
+            //     line, max_digit, second_max_digit, r
+            // );
+
+            r
+        })
+        .sum::<u16>() as i64
 }
--- src/b03.rs
+++ src/b03.rs
@@ -1,5 +1,5 @@
 pub const SAMPLE_OUTPUT: i64 = 3121910778619;
-
+pub const REAL_OUTPUT: i64 = 171388730430281;
 /*
 987654321111111
 811111111111119
@@ -7,7 +7,7 @@ pub const SAMPLE_OUTPUT: i64 = 3121910778619;
 818181911112111
 */
 
-pub fn run(inp: &str) -> i64 {
+pub fn run_naive(inp: &str) -> i64 {
     inp.lines()
         // .skip(1)
         .map(|line| {
@@ -39,7 +39,53 @@ pub fn run(inp: &str) -> i64 {
                 .map(|c| c.to_digit(10).unwrap() as u64)
                 .fold(0, |acc, d| acc * 10 + d);
 
-            println!("line: {}, digits: {:?}, r: {}", line, digits, r);
+            // println!("line: {}, digits: {:?}, r: {}", line, digits, r);
+
+            r
+        })
+        .sum::<u64>() as i64
+}
+
+pub fn run(inp: &str) -> i64 {
+    let lines: &[[u8; 101]; 200] = unsafe {
+        inp.as_bytes()
+            .as_chunks_unchecked::<101>()
+            .try_into()
+            .unwrap_unchecked()
+    };
+
+    lines
+        .iter()
+        .map(|line| {
+            let mut max_digit_index = 0;
+            let mut digits = [0; 12];
+
+            for (digit_n, digit_dest) in digits.iter_mut().enumerate() {
+                let mut max_digit = b'0';
+                let skipped = max_digit_index;
+                let taking = line.len() - (11 - digit_n) - skipped - 1;
+                // println!(
+                //     "digit_n: {}, skipped: {}, taking: {}",
+                //     digit_n, skipped, taking
+                // );
+
+                for (ind, c) in line.iter().skip(skipped).take(taking).enumerate() {
+                    if *c > max_digit {
+                        max_digit = *c;
+                        max_digit_index = ind + skipped;
+                    }
+                }
+
+                *digit_dest = max_digit;
+                max_digit_index += 1;
+            }
+
+            let r = digits
+                .iter()
+                .map(|c| (c - b'0') as u64)
+                .fold(0, |acc, d| acc * 10 + d);
+
+            // println!("line: {}, digits: {:?}, r: {}", line, digits, r);
 
             r
         })
