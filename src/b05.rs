a50dd844443f1920843e9c94a486f8f432140915
red <red@kinda.red>
2025-12-05 19:12:21 +0100

    b5 fastish

--- src/b05.rs
+++ src/b05.rs
@@ -1,14 +1,6 @@
 pub const SAMPLE_OUTPUT: i64 = 14;
 
-/*
-3-5
-10-14
-16-20
-12-18
-
-*/
-
-pub fn run(inp: &str) -> i64 {
+pub fn run_naive(inp: &str) -> i64 {
     let mut ranges = vec![];
 
     for line in inp.lines() {
@@ -28,6 +20,103 @@ pub fn run(inp: &str) -> i64 {
 
     // range index, count of overlapping ranges
     let mut range_counts: Vec<(i64, i16)> = Vec::with_capacity(ranges.len() * 2);
+
+    for (range_start, range_end) in ranges {
+        // binsearch index to insert start
+        let res = range_counts.binary_search_by(|(a, _)| a.cmp(&range_start));
+        if let Err(r) = res {
+            range_counts.insert(r, (range_start, 1));
+        } else {
+            range_counts[res.unwrap()].1 += 1;
+        }
+
+        let res = range_counts.binary_search_by(|(a, _)| a.cmp(&range_end));
+        if let Err(r) = res {
+            range_counts.insert(r, (range_end, -1));
+        } else {
+            range_counts[res.unwrap()].1 -= 1;
+        }
+    }
+
+    // count where there is at least one range overlapping
+    let mut sum_valid = 0;
+    let mut range_count = 0;
+    let mut previous_point = 0;
+
+    for (point, range_mod) in range_counts {
+        if range_count > 0 {
+            sum_valid += point - previous_point;
+        }
+
+        previous_point = point;
+
+        range_count += range_mod;
+        debug_assert!(range_count >= 0);
+    }
+
+    sum_valid
+}
+
+/*
+3-5
+10-14
+16-20
+12-18
+
+*/
+
+pub fn run(inp: &str) -> i64 {
+    let mut ranges: smallvec::SmallVec<[(i64, i64); 256]> = smallvec::SmallVec::new();
+
+    // for line in inp.lines() {
+    //     if line.is_empty() {
+    //         break;
+    //     }
+    //     let mut parts = line.split('-');
+    //     let start: i64 = parts.next().unwrap().parse().unwrap();
+    //     let end: i64 = parts.next().unwrap().parse().unwrap();
+
+    //     debug_assert!(end >= start);
+
+    //     ranges.push((start, end + 1));
+    // }
+
+    let mut input_bytes = inp.as_bytes().iter();
+    let mut char = *input_bytes.next().unwrap();
+    loop {
+        if char == b'\n' {
+            char = *input_bytes.next().unwrap();
+            if char == b'\n' {
+                break;
+            }
+        }
+
+        // let mut parts = inp.split('-');
+        // let start: i64 = parts.next().unwrap().parse().unwrap();
+        // let end: i64 = parts.next().unwrap().parse().unwrap();
+
+        let mut start = 0;
+        while char != b'-' {
+            start = start * 10 + (char - b'0') as i64;
+            char = *input_bytes.next().unwrap();
+        }
+        char = *input_bytes.next().unwrap(); // skip '-'
+
+        let mut end = 0;
+        while char != b'\n' {
+            end = end * 10 + (char - b'0') as i64;
+            char = *input_bytes.next().unwrap();
+        }
+
+        debug_assert!(end >= start);
+        ranges.push((start, end + 1));
+    }
+
+    // ranges.sort();
+
+    // range index, count of overlapping ranges
+    // let mut range_counts: Vec<(i64, i16)> = Vec::with_capacity(ranges.len() * 2);
+    let mut range_counts: smallvec::SmallVec<[(i64, i16); 512]> = smallvec::SmallVec::with_capacity(ranges.len() * 2);
 
     for (range_start, range_end) in ranges {
         // binsearch index to insert start
