22fe78cd2fb56b2e0fe1e06e6d927a4edaf2aa37
red <red@kinda.red>
2025-12-06 16:11:34 +0100

    fast ish b6

--- src/b06.rs
+++ src/b06.rs
@@ -6,42 +6,39 @@ pub const SAMPLE_OUTPUT: i64 = 3263827;
   6 98  215 314
 *   +   *   +
 */
-pub fn run(inp: &str) -> i64 {
-    let mut total_sum = 0;
+pub fn run_naive(inp: &str) -> i64 {
+    let mut total_sum: u64 = 0;
 
-    let lines = inp.lines().collect::<Vec<&str>>();
+    let lines = inp.lines().map(|l| l.as_bytes()).collect::<Vec<&[u8]>>();
 
     let longest_line_len = lines.iter().map(|l| l.len()).max().unwrap();
 
-    let mut operator = '+';
-    let mut num_acc = 0;
+    let mut operator = b'+';
+    let mut num_acc: u64 = 0;
     for col in 0..longest_line_len {
-        if matches!(
-            lines.last().unwrap().chars().nth(col).unwrap_or(' '),
-            '+' | '*'
-        ) {
+        if matches!(lines.last().unwrap().get(col).unwrap_or(&b' '), b'+' | b'*') {
             total_sum += num_acc;
-            operator = lines.last().unwrap().chars().nth(col).unwrap();
+            operator = *lines.last().unwrap().get(col).unwrap();
             match operator {
-                '+' => num_acc = 0,
-                '*' => num_acc = 1,
+                b'+' => num_acc = 0,
+                b'*' => num_acc = 1,
                 _ => unreachable!(),
             }
         }
 
         let mut col_nums = vec![];
         for row in 0..lines.len() - 1 {
-            let c = lines[row].chars().nth(col).unwrap_or(' ');
-            if c == ' ' {
+            let c = lines[row].get(col).unwrap_or(&b' ');
+            if *c == b' ' {
                 continue;
             }
             col_nums.push(c);
         }
 
         // convert to decimal number
-        let mut n = 0;
+        let mut n: u64 = 0;
         for c in col_nums {
-            n = n * 10 + (c as i64 - '0' as i64);
+            n = n * 10 + (*c - b'0') as u64;
         }
 
         if n == 0 {
@@ -49,13 +46,68 @@ pub fn run(inp: &str) -> i64 {
         }
 
         match operator {
-            '+' => num_acc += n,
-            '*' => num_acc *= n,
+            b'+' => num_acc += n,
+            b'*' => num_acc *= n,
             _ => unreachable!(),
         }
     }
 
     total_sum += num_acc;
 
-    total_sum
+    total_sum as i64
+}
+
+pub fn run(inp: &str) -> i64 {
+    let mut total_sum: u64 = 0;
+    let inp = inp.as_bytes();
+
+    let line_length = inp.iter().position(|&c| c == b'\n').unwrap() + 1; // include newline
+    let line_count = inp.len() / line_length;
+
+    let index = |line, col| line * line_length + col;
+
+    let mut operator = b'+';
+    let mut num_acc: u64 = 0;
+    for col in 0..line_length - 1 {
+        // let local_operator = inp[index(line_count - 1, col)];
+        let local_operator = unsafe { *inp.get_unchecked(index(line_count - 1, col)) };
+
+        match local_operator {
+            b'+' => {
+                total_sum += num_acc;
+                operator = local_operator;
+                num_acc = 0
+            }
+            b'*' => {
+                total_sum += num_acc;
+                operator = local_operator;
+                num_acc = 1
+            }
+            _ => {}
+        }
+
+        let mut n: u64 = 0;
+        for row in 0..line_count - 1 {
+            // let c = inp[index(row, col)];
+            let c = unsafe { *inp.get_unchecked(index(row, col)) };
+            if c == b' ' {
+                continue;
+            }
+            n = n * 10 + (c - b'0') as u64;
+        }
+
+        if n == 0 {
+            continue;
+        }
+
+        match operator {
+            b'+' => num_acc += n,
+            b'*' => num_acc *= n,
+            _ => unreachable!(),
+        }
+    }
+
+    total_sum += num_acc;
+
+    total_sum as i64
 }
